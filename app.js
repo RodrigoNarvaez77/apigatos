086807fa52df79dc3f73604c6c393daec6172083
RodrigoNarvaez77 <rodrigo.narvaez.munoz@ciisa.cl>
2024-11-28 13:51:07 -0300

    Cambios 28-11-2024

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const helmet = require('helmet');
 const catRoutes = require('./routes/catroutes'); // Importa las rutas
 const PORT = process.env.PORT || 3010;
 
@@ -11,6 +12,9 @@ app.use(express.static(path.join(__dirname, './View')));
 // Endpoint para cargar datos de una API
 //ruta del archivo
 app.use('/api/data',catRoutes);
+
+// Usa Helmet para proteger tu app
+app.use(helmet());
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
